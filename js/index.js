50db1ac42aa27e63cac0b67811bca5e8635e123a
darvinh12 <arondon33@gmail.com>
2026-05-19 21:03:54 -0400

    Listado de perfiles dinamicos usando javascript

--- js/index.js
+++ js/index.js
@@ -13,4 +13,34 @@ function initConfig() {
     document.getElementById("footer").textContent = config.copyRight;
 }
 
+function renderProfiles() {
+    const contenedor = document.getElementById("perfiles");
+    contenedor.innerHTML = "";
+
+    profiles.forEach(function(perfil) {
+        const tarjeta = document.createElement("a");
+        tarjeta.className = "tarjeta-perfil";
+        tarjeta.href = "#";
+
+        tarjeta.innerHTML = `
+            <div class="contenedor-img-perfil">
+                <img src="./${perfil.ci}/${perfil.ci}Big${perfil.image_ext}" alt="${perfil.name}" class="imagen-perfil">
+            </div>
+            <div class="contenedor-info-perfil">
+                <h3>${perfil.name}</h3>
+            </div>
+            <div class="barra-inferior"></div>
+        `;
+
+        tarjeta.addEventListener("click", function(e) {
+            e.preventDefault();
+            window.location.href = `./profile.html?ci=${perfil.ci}`;
+        });
+
+        contenedor.appendChild(tarjeta);
+    });
+}
+
 initConfig();
+renderProfiles();
+
